1bfec201f0333a08d96d4332428d86a7820a6314
Thomas Holmes <shadox@gmail.com>
2017-12-30 21:32:56 -0500

    Fix negative resource regen. Somehow the change got reverted :(

--- game/resource.go
+++ game/resource.go
@@ -2,6 +2,7 @@ package main
 
 import "math"
 
+// Resource represents some character resource that hax a max and [re/de]generates over time
 type Resource struct {
 	RegenRate float64
 
@@ -11,11 +12,16 @@ type Resource struct {
 	regenPartial float64
 }
 
+// Tick applies the regen rate to the resource
 func (resource *Resource) Tick() {
 	resource.regenPartial += resource.RegenRate
 
-	if math.Floor(resource.regenPartial) >= 1 {
-		resource.Current = min(int(math.Floor(resource.regenPartial))+resource.Current, resource.Max)
-		resource.regenPartial -= math.Floor(resource.regenPartial)
+	if math.Floor(resource.regenPartial) == math.Ceil(resource.regenPartial) {
+		adjustment := math.Floor(resource.regenPartial)
+		newValue := resource.Current + int(math.Floor(resource.regenPartial))
+		newValue = min(newValue, resource.Max)
+		newValue = max(newValue, 0)
+		resource.Current = newValue
+		resource.regenPartial -= adjustment
 	}
 }
