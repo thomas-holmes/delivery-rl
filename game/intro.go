bb5b3936112003aacfad5f17f93dfd38146117b0
Thomas Holmes <shadox@gmail.com>
2017-12-31 00:47:19 -0500

    Add pizza ascii art

--- game/intro.go
+++ game/intro.go
@@ -1,10 +1,20 @@
 package main
 
-import "github.com/thomas-holmes/gterm"
-import "github.com/veandco/go-sdl2/sdl"
+import (
+	"bufio"
+	"io"
+	"log"
+	"os"
+	"path"
+
+	"github.com/thomas-holmes/gterm"
+	"github.com/veandco/go-sdl2/sdl"
+)
 
 type IntroScreen struct {
 	PopMenu
+
+	pizza []string
 }
 
 func (intro *IntroScreen) Update(input InputEvent) bool {
@@ -15,14 +25,52 @@ func (intro *IntroScreen) Update(input InputEvent) bool {
 	return true
 }
 
+func (intro *IntroScreen) maybeLoadPizza() {
+	if len(intro.pizza) > 0 {
+		return
+	}
+
+	file, err := os.Open(path.Join("assets", "art", "pizza.txt"))
+	if err != nil {
+		log.Panicln("Could not load opening art", err)
+	}
+
+	buf := bufio.NewReader(file)
+
+	for {
+		line, err := buf.ReadString('\n')
+		if err == io.EOF {
+			intro.pizza = append(intro.pizza, line)
+			break
+		} else if err != nil {
+			log.Panicln("Failed during reading of pizza", err)
+		}
+		intro.pizza = append(intro.pizza, line)
+	}
+}
+
+func (intro *IntroScreen) drawPizza(window *gterm.Window) {
+	x, y := 27, 2
+	for _, line := range intro.pizza {
+		if err := window.PutString(x, y, line, White); err != nil {
+			log.Panicln("Couldn't draw pizza", err)
+		}
+		y++
+	}
+}
+
 func (intro *IntroScreen) Render(window *gterm.Window) {
 	window.ClearWindow()
 
+	intro.maybeLoadPizza()
+
+	intro.drawPizza(window)
+
 	content := "DeliveryRL"
-	x, y := (window.Columns-len(content))/2, window.Rows/2
+	x, y := (window.Columns-len(content))/2, window.Rows/2+7
 	window.PutString(x, y, "DeliveryRL", LightBlue)
 
 	content = "Press any key to begin..."
-	x, y = (window.Columns-len(content))/2, window.Rows/2+1
+	x, y = (window.Columns-len(content))/2, y+1
 	window.PutString(x, y, "Press any key to begin...", Grey)
 }
