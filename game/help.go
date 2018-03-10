9dca719a259a90989aaed802db743abd5a85a1a3
Thomas Holmes <shadox@gmail.com>
2018-03-10 16:53:42 -0500

    Add some objective text

--- game/help.go
+++ game/help.go
@@ -49,4 +49,24 @@ func (pop HelpPop) Render(window *gterm.Window) {
 		window.PutString(x, y, fmt.Sprintf("%-20s %-30s", m.Name, strings.Join(padded, " ")), White)
 		y++
 	}
+
+	y += 2
+	objective1 := "Welcome to DeliveryRL! You are a typical delivery person for a most unusual pizza shop. " +
+		"Sometimes your store gets orders from mythical creatures. Today an Ancient Dragon has ordered a pizza and expects " +
+		"it to be delivered promptly, and warm! Why does a dragon need delivery? It's not your job to ask those questions, " +
+		"but you drew the short straw this time."
+
+	y += putWrappedText(window, objective1, x, y, 2, 0, pop.W-3, White)
+	y++
+
+	objective2 := "Race to the depths of the caverns and deliver the pizza to the Dragon who is waiting. " +
+		"Survive by avoiding monsters, fighting for your life, and distracting them with some extra food " +
+		"you brought along. Maybe you can scrounge up something useful from past adventurers, " +
+		"but remember: you're no warrior!"
+
+	y += putWrappedText(window, objective2, x, y, 2, 0, pop.W-3, White)
+	y++
+
+	window.PutString(x, y, "Good Luck!", White)
+
 }
