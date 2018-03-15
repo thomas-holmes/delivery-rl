a49064038867f3bdfd69f69949b852fe1ea28667
Thomas Holmes <shadox@gmail.com>
2018-03-14 21:09:09 -0400

    Add intro screen menu items, flash on current selection.

--- game/intro.go
+++ game/intro.go
@@ -4,9 +4,12 @@ import (
 	"bufio"
 	"io"
 	"log"
+	"math"
 	"os"
 	"path"
 	"strings"
+
+	"github.com/veandco/go-sdl2/sdl"
 
 	"github.com/thomas-holmes/delivery-rl/game/controls"
 	"github.com/thomas-holmes/gterm"
@@ -15,11 +18,28 @@ import (
 type IntroScreen struct {
 	PopMenu
 
+	activeChoice int
+	timeFloor    uint32
+
 	splash []string
+}
+
+func (intro *IntroScreen) adjustSelectionWrap(delta int) {
+	intro.timeFloor = sdl.GetTicks()
+	intro.activeChoice += delta
+	if intro.activeChoice < 0 {
+		intro.activeChoice = max(0, len(menuChoices)-1)
+	} else if intro.activeChoice >= len(menuChoices) {
+		intro.activeChoice = 0
+	}
 }
 
 func (intro *IntroScreen) Update(action controls.Action) {
 	switch action {
+	case controls.Up:
+		intro.adjustSelectionWrap(-1)
+	case controls.Down:
+		intro.adjustSelectionWrap(1)
 	case controls.Confirm:
 		fallthrough
 	case controls.Cancel:
@@ -61,12 +81,54 @@ func (intro *IntroScreen) drawSplash(window *gterm.Window) {
 	}
 }
 
+var menuChoices = []string{
+	"Start Game",
+	"Quit Game",
+}
+
+func (intro *IntroScreen) drawMenuItems(window *gterm.Window) {
+	ticks := sdl.GetTicks()
+	if intro.timeFloor == 0 {
+		intro.timeFloor = ticks
+	}
+	time := (ticks - intro.timeFloor) / 10.0
+	angle := float64(time%360) * (math.Pi / 180.0)
+	cos := math.Cos(angle)
+
+	r, g, b := 127, 127, 127
+
+	r = int(float64(r) + (float64(127 * cos)))
+	g = int(float64(g) + (float64(127 * cos)))
+	b = int(float64(b) + (float64(127 * cos)))
+
+	activeBg := sdl.Color{R: uint8(r), G: uint8(g), B: uint8(b), A: 255}
+	inactiveBg := gterm.NoColor
+
+	y := window.Rows - 10
+	for i, content := range menuChoices {
+		var bg sdl.Color
+		if i == intro.activeChoice {
+			bg = activeBg
+		} else {
+			bg = inactiveBg
+		}
+
+		x := (window.Columns - len(content)) / 2
+
+		window.PutStringBg(x, y, content, White, bg)
+		y++
+	}
+
+}
+
 func (intro *IntroScreen) Render(window *gterm.Window) {
 	window.ClearWindow()
 
 	intro.maybeLoadSplash()
 
 	intro.drawSplash(window)
+
+	intro.drawMenuItems(window)
 
 	content := "DeliveryRL"
 	x, y := (window.Columns-len(content))/2, window.Rows-5
